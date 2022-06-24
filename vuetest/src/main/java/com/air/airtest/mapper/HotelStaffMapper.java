7d57fa9f000b8ce6d87b75aa9eeed9294cabc00e
wuhufly <wuhufly@git.com>
2022-06-24 12:26:37 +0800

    重新开始。。。

---
+++ vuetest/src/main/java/com/air/airtest/mapper/HotelStaffMapper.java
@@ -0,0 +1,11 @@
+package com.air.airtest.mapper;
+
+import com.air.airtest.entity.HotelStaff;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HotelStaffMapper extends BaseMapper<HotelStaff>{
+    //HotelStaff selectHotelStaff(String hotelstaffno);
+}
