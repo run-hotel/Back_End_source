4ed2a17cf82a3156bc35429491c470f66262445d
zhaodaodao <87349308+zhaodaodao@users.noreply.github.com>
2022-06-25 10:53:55 +0800

    zfb1.0

---
+++ vuetest/src/main/java/com/air/airtest/controller/PayController.java
@@ -0,0 +1,42 @@
+package com.air.airtest.controller;
+
+import com.alipay.api.AlipayApiException;
+import com.air.airtest.model.AlipayBean;
+import com.air.airtest.service.PayService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pay")
+@CrossOrigin
+public class PayController {
+
+    @Resource
+    private PayService payService;
+
+    /**
+     * 阿里支付
+     * @param alipayBean
+     * @return
+     * @throws AlipayApiException
+     */
+    @PostMapping("/alipay")
+    public Map alipay(AlipayBean alipayBean) throws AlipayApiException {
+        System.out.println(alipayBean);
+        Map<String,Object> map = new HashMap<String,Object>();
+
+
+        String str = payService.aliPay(alipayBean);
+        System.out.println(str);
+        map.put("msg",str);
+        map.put("code",0);
+//
+        return map;
+    }
+}
---
+++ vuetest/src/main/java/com/air/airtest/service/PayService.java
@@ -0,0 +1,22 @@
+package com.air.airtest.service;
+
+import com.alipay.api.AlipayApiException;
+import com.air.airtest.model.AlipayBean;
+
+/**
+ * 支付服务
+ * @author zhaodaodao
+ * @date 2022-06-22
+ */
+
+public interface PayService {
+
+    /**
+     * 支付宝支付接口
+     * @param alipayBean
+     * @return
+     * @throws AlipayApiException
+     */
+    String aliPay(AlipayBean alipayBean) throws AlipayApiException;
+
+}
